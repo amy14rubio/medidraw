b40b690221f3e56bc77ef72d566981254a07cb6d
Amyruth Rubio <amy14rubio@gmail.com>
2023-06-26 14:51:22 -0400

    Initial Commit

---
+++ medidraw/medidrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  medidrawApp.swift
+//  medidraw
+//
+//  Created by Amyruth Rubio on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct medidrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

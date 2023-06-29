e102bb171a56b296fd1dc468a04f47a38a83cb1c
Moyosore Ogunkoya <momoart188@gmail.com>
2023-06-29 12:58:42 -0400

    Added sound. Note: Currently only one song is playing. I'm not sure how to make it so that multiple songs play after one button is pushed.

--- medidraw/ContentView.swift
+++ medidraw/ContentView.swift
@@ -6,10 +6,44 @@
 //
 
 import SwiftUI
+import AVKit
+
+
+// Play music
+class SoundManager {
+    static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    
+    enum SoundOption: String {
+        case silentWood
+        case CrescentMoon
+    }
+    
+    
+    
+    func playSound(sound: SoundOption) {
+        
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+            
+        }
+    }
+}
+
+
+
 
 struct ContentView: View {
     
     var body: some View {
+        
         NavigationStack {
             ZStack {
                 //green background color
@@ -17,6 +51,12 @@ struct ContentView: View {
                     .ignoresSafeArea()
       
                 VStack (spacing: 20.0){
+                    Button("Play Sound") {
+                        SoundManager.instance.playSound(sound : .CrescentMoon) //calls to play the sound
+                        // Note: As of right now, the button only plays one song, not 100% sure how to fix that. - Moyo
+                    }
+                    
+                    
                     //title
                     Text("Medidraw")
                         .font(.custom("Bauhaus93", size: 50))
